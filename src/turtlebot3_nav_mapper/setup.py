1eb085024f37760eb563b0fb84454df904445064
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2023-11-10 15:22:56 -0300

    Workspace criado e launch file de mapeamento

---
+++ src/turtlebot3_nav_mapper/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'turtlebot3_nav_mapper'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lyo.quintao',
+    maintainer_email='99191948+lyorrei-inteli@users.noreply.github.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
