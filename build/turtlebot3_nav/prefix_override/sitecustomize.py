b07d7570e9d6ca67f1d4b53a808d4905f3c8e932
Lyorrei Shono <lyorrei.quintao@sou.inteli.edu.br>
2023-11-12 20:43:06 -0300

    Update nav script

--- build/turtlebot3_nav/prefix_override/sitecustomize.py
+++ build/turtlebot3_nav/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/Users/lyo.quintao/Documents/development/Projects/ponderada2_mod8/install/turtlebot3_nav'
+sys.prefix = sys.exec_prefix = '/home/lyorrei/Documents/Projects/ponderada2_mod8/install/turtlebot3_nav'
