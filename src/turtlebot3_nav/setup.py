68c35f19ec17451efe5d4a0bccaf25e5c5ce63ef
Lyorrei Shono Quintão <99191948+lyorrei-inteli@users.noreply.github.com>
2023-11-12 19:41:16 -0300

    Created nav script

--- src/turtlebot3_nav/setup.py
+++ src/turtlebot3_nav/setup.py
@@ -24,6 +24,8 @@ setup(
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [],
+        "console_scripts": [
+          "nav=turtlebot3_nav.turtlebot3_nav:main"
+        ],
     },
 )
